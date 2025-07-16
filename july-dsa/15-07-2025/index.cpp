2c666ace42387f90de6ce11cb9390f65250ffeff
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-07-16 10:53:31 +0530

    Added Code for July 15

---
+++ july-dsa/15-07-2025/index.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+
+int sum(int a,int b)
+{
+    cout<<"A - B => "<<a-b<<endl;
+    return a-b;
+}
+
+int main(){
+    // Variables -?? It is a kind of reserved space in the memory 
+    // Int 
+    // Char
+    // Float
+    // String Lateral- but the group of charachters.
+    // int x=10;
+    // cout<<x<<endl;
+    // Loops.
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+    // cout<<"Pratham Saxena"<<endl;
+
+    // for(int index=0;index>1000;index++)
+    // {
+    //     cout<<"Pratham Saxena - "<<index<<endl;
+    // }
+    // // While and Do while
+    // // 
+    sum(19,18);
+
+    // int age=19;
+
+    // if(age>=18)
+    // {
+    //     cout<<"You can drive the car!!"<<endl;
+    // }
+    // else{
+    //     cout<<"Car ko haaath mat lagaaaaa!!!"<<endl;
+    // }
+
+    // for(int index=2;index<100;index++)
+    // {
+    //     if(index%2==0)
+    //     {
+    //         cout<<index<<endl;
+    //     }
+    // }
+
+    sum(2,8);
+
+    // Functions
+    // 
+    return 0;
+}
