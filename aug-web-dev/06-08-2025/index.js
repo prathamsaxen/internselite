9e12f4dd421b2b79ffce556ac39a9fdaf2220c3f
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-09-08 23:20:36 +0530

    Added Code

--- aug-web-dev/06-08-2025/index.js
+++ aug-web-dev/06-08-2025/index.js
@@ -1,12 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get('/test',(req,res)=>{
-    res.send("Server is Healthy");
-})
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
-app.listen(8000,()=>{
-    console.log(`Server Is Established over PORT number = 8000`);
-})
+app.get("/test", (req, res) => {
+  res.send("Server is Healthy");
+});
+
+app.post("/data", (req, res) => {
+  console.log("Request Hit");
+  console.log(req.body);
+  res.send("POST HIT Successfully");
+});
+
+app.delete("/delete/:id", (req, res) => {
+  console.log(req?.params?.id);
+  res.send("Data Element Deleted");
+});
+
+app.listen(8000, () => {
+  console.log(`Server Is Established over PORT number = 8000`);
+});
 
 // nodemon?
+// CRUD Operations
+// 1. CREATE - POST
+// 2. READ - GET
+// 3. UPDATE - PATCH | PUT
+// 4. DELETE - DELETE
+// How to Pass Data from frontend to backend?
+// BodyParser
+// Schemas
+// MongoDB
