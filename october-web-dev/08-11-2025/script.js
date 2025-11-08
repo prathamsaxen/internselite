02f13024200e98cb35bce581e6cd2e638b531ff9
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-11-08 17:53:05 +0530

    Basic of JS Implemented

---
+++ october-web-dev/08-11-2025/script.js
@@ -0,0 +1,107 @@
+// // Hello World - Print "cout", "printf", "print", system.outln, console.log()
+// // console.log("Pratham Saxena");
+
+// // // Variables - int, const, float, double, boolean, char, etc
+// // // JavaScript - let and var and const
+
+// // let data = "Pratham Saxena";
+// // let isEngineer = true;
+
+// // console.log(data);
+// // console.log(isEngineer);
+
+// // const fruit = "apple";
+// // console.log(fruit);
+
+// // //
+
+// // let age = 19;
+
+// // if(age<18)
+// // {
+// //     console.log("User Can not drive a car");
+// // }
+// // else if(age === 18)
+// // {
+// //     console.log("User can learn driving a car");
+// // // }
+// // // else if(age>18)
+// // // {
+// // //     console.log("User can drive a car");
+// // // }
+
+// // // Loops -
+// // // for(let index=0;index<=10;index++)
+// // // {
+// // //     console.log(index);
+// // // }
+
+// // // const data = 'Hello';
+// // // let age = 10;
+// // // console.log(typeof data);
+// // // console.log(typeof age);
+
+// // function add(a,b)
+// // {
+// //     return a+b;
+// // }
+
+// // console.log(add(5,4));
+// // console.log(add(10,5));
+
+// // // Print Even Numbers
+// // //
+
+// // function checkEven(number){
+// //     if(number%2===0)
+// //     {
+// //         return true;
+// // //     }
+// // //     return false;
+// // // }
+
+// // const checkEven = (number) => {
+// //   if (number % 2 === 0) {
+// //     return true;
+// //   }
+// //   return false;
+// // };
+
+// // for(let index=0;index<100;index++)
+// // {
+// //    if(checkEven(index))
+// //    {
+// //     console.log(index);
+// //    }
+// // }
+
+// // // Function to Print Factorial?
+// // let data = 5;
+// // let Factorial = 1;
+
+// // while (data > 1) {
+// //   Factorial *= data;
+// //   data--;
+// // }
+
+// // console.log(Factorial);
+
+// // const data = {
+// //     name:"Pratham Saxena",
+// //     occupation:"Software Engineer",
+// //     mobileNumber:8077761461,
+// //     instagram:'prathamsaxen'
+// // };
+// // console.log(data);
+// // console.log(data.mobileNumber);
+
+// const data = ["Pratham Saxena", 90456, true];
+// console.log(data);
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// // numbers = numbers.map((item) => item * 2);
+// // Reduce - Method
+// console.log(numbers.filter(item => item % 2 === 0));
+
+// console.log(numbers);
