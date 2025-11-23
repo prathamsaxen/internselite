e6e6dab3a70dec82f54275359e9d905bd76734d8
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-11-23 20:07:19 +0530

    Implemented

--- october-web-dev/15-11-2025/src/App.jsx
+++ october-web-dev/15-11-2025/src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import NameCard from "./Components/NameCard";
-
 function App() {
   const [count, setCount] = useState(0);
-  const [isMale,setIsMale]=useState(true);
+  const [theme, setTheme] = useState("Light");
+  const names = ["Pratham", "Jay", "Sujal", "Rohan"];
+  // Access  Update
+  // State is a kind of replacement of variables.
+
   const increament = () => {
     setCount(count + 1);
   };
-  const decrement = () => {
+  const decremeant = () => {
     setCount(count - 1);
   };
+  const switchTheme = () => {
+    if (theme == "Dark") {
+      setTheme("Light");
+    } else {
+      setTheme("Dark");
+    }
+  };
+
+  useEffect(()=>{
+    alert("First Time Component Mounts!");
+  },[]);
 
   return (
-    <div className="container">
-      {/* <button onClick={decrement}>Minus</button>
-      <p>{count}</p>
-      <button onClick={increament}>Plus</button> */}
-      <NameCard name="Pratham Saxena"/>
-      <NameCard name="Pihu Mahajan"/>
-      <NameCard name="Vishakha Sahu"/>
-
-      <button onClick={()=>setIsMale(!isMale)}>Gender</button>
-
-      {isMale?<h1>Hello Sir,</h1>:<h1>Hello Mam</h1>}
-
-
+    <div className={`top-wrapper ${theme == "Dark" ? "dark-theme" : "light-theme"}`}>
+      {/* <div className={`container ${theme == "Dark" ? "dark-theme" : "light-theme"}`}>
+        <div className="wrapper">
+          <button onClick={decremeant}>Minus</button>
+          {count}
+          <button onClick={increament}>Plus</button>
+        </div>
+        <button onClick={switchTheme}>
+          {theme == "Dark" ? "Turn the Theme Light" : "Dark the theme"}
+        </button>
+      </div> */}
+      <ul>
+        {names.map((item, index) => {
+          return  <NameCard key={index} name={item}/>;
+        })}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+// COnditional Rendering - It will check the truth value and only renders the content which you want?
