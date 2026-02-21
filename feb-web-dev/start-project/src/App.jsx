5637735c4f6c6d4da70fea48b40c6bfd8545552f
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2026-02-21 19:55:37 +0530

    Added Assignment

--- feb-web-dev/start-project/src/App.jsx
+++ feb-web-dev/start-project/src/App.jsx
@@ -31,3 +31,15 @@ function App(){
 }
 
 export default App;
+
+//Assignment:
+// 1. Create a new component called NameList.
+// 2. Create a new state called nameList.
+// 3. Create a new function called updateNameList.
+// 4. Create a new button called Add Name.
+// 5. Create a new input field called Name.
+// 6. Create a new button called Add Name.
+// 7. Create a new button called Remove Name.
+// 8. Create a new button called Remove All Names.
+// 9. Create a new button called Update Name.
+// 10. Create a new button called Update All Names.
