238472724bb2c2dae0b5b0bcea9308f5f4e135b0
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2026-09-07 19:55:08 +0530

    Code Ready

---
+++ sep-dsa-batch/07-september-2026/index.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int data = 10;
+    // cout<<"Enter your age? "<<endl;
+    // cin>>number;
+    // cout<<"Your age is: "<<number<<endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
